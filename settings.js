7058f5244f59ac644a35ed6809b1c4eac24767b4
skc-sketch112 <chunnachud@gmail.com>
2025-08-07 11:52:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "nFgimLaS#53kxa4THCFsW-Exv1d3ZcAzOHHccKbKn7xe270Zu3SI", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
